c1070e56c3e389a461204af6e9f11b9f481da4f8
jonathanbrito48 <jonathanbrito48@gmail.com>
2023-07-12 16:57:27 -0300

    Projetos Python

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+    1: {"Nome": "Nebulosa de Carina",
+        "Legenda":"Webbtelescope.org / NASA / James Webb"},
+    2: {"Nome": "Galaxia NGC 1079",
+        "Legenda": "nasa.org / NASA / Hubble"}
+}        
+    return render(request, 'galeria/index.html',{"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
